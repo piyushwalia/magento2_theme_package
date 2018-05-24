d8ac25d9360b0a5fc57a9a0bb43a537597a39b64
pwcueblocks <walia11127@gmail.com>
2018-05-25 01:48:03 +0530

    Prints error when adding wrong maagento version and fixing bug, when user add wrong version, it does'nt download theme files

--- magento_theme.py
+++ magento_theme.py
@@ -15,6 +15,7 @@ theme_folder_path = os.path.join(magento_dir_path,vendor_name, theme_name)
 if not os.path.exists(magento_dir_path):
     # this wil create dir    
     os.makedirs(magento_dir_path)
+        
 # if the above path exsist's, then it join the above dir path, vendor name and theme name    
 if not os.path.exists(theme_folder_path):
     # this will create dir with vendor name, theme name and magento root path to theme
@@ -33,17 +34,24 @@ if not os.path.exists(theme_folder_path):
     for themefolder in theme_folders:        
         os.makedirs(os.path.join(theme_folder_path,themefolder))    
 
+  
+def download_theme_files():
     # Magento version
     version_input = raw_input('Magento Version:')
     # Download default theme file from Magento latest version URL
     downloadable_files = [ 'theme.xml', 'composer.json',  'registration.php', 'etc/view.xml']
-    for url_folders in downloadable_files: 
-        magento_file_url = 'https://raw.githubusercontent.com/magento/magento2/'+ version_input +'-develop/app/design/frontend/Magento/blank/'
-        theme_xml = os.path.join(magento_file_url + url_folders)
-        print "Downloading....", url_folders    
-        filedata = urllib2.urlopen(theme_xml)  
-        datatowrite = filedata.read()
-        with open(os.path.join(theme_folder_path, url_folders) ,'wb') as f:  
-            f.write(datatowrite) 
-        
-    
+    for url_folders in downloadable_files:         
+        magento_file_url = 'https://raw.githubusercontent.com/magento/magento2/'+ version_input +'-develop/app/design/frontend/Magento/blank/'                
+        try:
+            theme_xml = os.path.join(magento_file_url + url_folders)
+            print "Downloading...........", url_folders    
+            filedata = urllib2.urlopen(theme_xml)          
+            datatowrite = filedata.read()
+            with open(os.path.join(theme_folder_path, url_folders) ,'wb') as f:  
+                f.write(datatowrite)
+        except urllib2.HTTPError as err:
+            if err.code == 404:
+                print "Please enter Valid Magento Version" 
+                break                
+if __name__== "__main__":
+    download_theme_files()
