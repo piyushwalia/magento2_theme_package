4b5fd138cfc4fc60692e28938e7c6fb60b6bd112
Gopal Sharma <gopalindians@gmail.com>
2018-05-24 23:04:12 +0530

    Added Constants

--- magento_theme.py
+++ magento_theme.py
@@ -15,43 +15,53 @@ try:
 except ImportError:
     import urllib2
 
+# magento_file_url = 'https://raw.githubusercontent.com/magento/magento2/' + version_input + '-develop/app/design/frontend/Magento/blank/'
+
+REMOTE_URL_PART1 = 'https://raw.githubusercontent.com/magento/magento2/'
+REMOTE_URL_PART2 = '-develop/app/design/frontend/Magento/blank/'
+
+# this will create Inner folders of theme
+WEB_FOLDERS = ['css', 'Images']
+THEME_FOLDERS = ['media', 'etc', 'Magento_Theme']
+
+# Download default theme file from Magento latest version URL
+DOWNLOADABLE_FILES = ['theme.xml', 'composer.json', 'registration.php', 'etc/view.xml']
+
 # magento directory path which will is already present, if not, will create dir
-magento_dir_path = 'app/design/frontend/'
+MAGENTO_DIR_PATH = 'app/design/frontend/'
+
+
 # If you are using python3, change raw_input to input
 # Input from console where user will enter Vendor name
 try:
     vendor_name = raw_input('Enter vendor name(where you want your theme to be created):')
 except NameError:
     vendor_name = input('Enter vendor name (where you want your theme to be created):')
-# Input from console where user will enter theme name
 
+# Input from console where user will enter theme name
 try:
     theme_name = raw_input('Enter theme name (which you want to create):')
 except NameError:
     theme_name = input('Enter theme name (which you want to create):')
 # var which joins the magento dir path, vendor name and theme name
-theme_folder_path = os.path.join(magento_dir_path, vendor_name, theme_name)
+theme_folder_path = os.path.join(MAGENTO_DIR_PATH, vendor_name, theme_name)
 
 # if the magento directory path does not exsist, it will create the dir
-if not os.path.exists(magento_dir_path):
+if not os.path.exists(MAGENTO_DIR_PATH):
     # this wil create dir
-    os.makedirs(magento_dir_path)
+    os.makedirs(MAGENTO_DIR_PATH)
 # if the above path exsist's, then it join the above dir path, vendor name and theme name
 if not os.path.exists(theme_folder_path):
     # this will create dir with vendor name, theme name and magento root path to theme
     os.makedirs(os.path.join(theme_folder_path))
 
-    # this will create Inner folders of theme
-    web_folders = ['css', 'Images']
-    theme_folders = ['media', 'etc', 'Magento_Theme']
-
     print('Do keep your theme preview Image in media folder')
 
     # web folders
-    for webfolder in web_folders:
+    for webfolder in WEB_FOLDERS:
         os.makedirs(os.path.join(theme_folder_path, 'web', webfolder))
         # theme folders
-    for themefolder in theme_folders:
+    for themefolder in THEME_FOLDERS:
         os.makedirs(os.path.join(theme_folder_path, themefolder))
 
     # Magento version
@@ -59,10 +69,8 @@ if not os.path.exists(theme_folder_path):
         version_input = raw_input('Magento Version:')
     except NameError:
         version_input = input('Magento Version:')
-    # Download default theme file from Magento latest version URL
-    downloadable_files = ['theme.xml', 'composer.json', 'registration.php', 'etc/view.xml']
-    for url_folders in downloadable_files:
-        magento_file_url = 'https://raw.githubusercontent.com/magento/magento2/' + version_input + '-develop/app/design/frontend/Magento/blank/'
+    for url_folders in DOWNLOADABLE_FILES:
+        magento_file_url = REMOTE_URL_PART1 + version_input + REMOTE_URL_PART2
         theme_xml = os.path.join(magento_file_url + url_folders)
         print("Downloading...." + url_folders)
         fileData = urllib2.urlopen(theme_xml)
